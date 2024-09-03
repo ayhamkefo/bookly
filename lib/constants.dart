fd2552ca2fe5cba88a37960d71d684288d8bbbb0
ayhamkefo <ayhamkefo048@gmail.com>
2024-09-03 19:58:59 +0300

    create folrders and files home page , refactor the initstate in the splach page

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,3 +1,4 @@
 import 'package:flutter/material.dart';
 
 const kPramaryColor = Color(0xFF100B20);
+const kDuration = Duration(milliseconds: 250);
--- lib/features/splach/presentation/views/widgets/splach_view_body.dart
+++ lib/features/splach/presentation/views/widgets/splach_view_body.dart
@@ -1,5 +1,8 @@
+import 'package:bookly/constants.dart';
+import 'package:bookly/features/home/presentation/views/home_view.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:get/get.dart';
 
 import '../../../../../core/utils/assets_data.dart';
 import 'sliding_animation.dart';
@@ -17,18 +20,15 @@ class _SplachViewBodyState extends State<SplachViewBody>
   late Animation<Offset> slidingAnimation;
   @override
   void initState() {
-    animationController =
-        AnimationController(vsync: this, duration: const Duration(seconds: 2));
-    slidingAnimation =
-        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
-            .animate(animationController);
-    animationController.forward();
+    initSlidingAnimation();
+    goToHome();
     super.initState();
   }
 
   @override
   void dispose() {
     animationController.dispose();
+
     super.dispose();
   }
 
@@ -48,5 +48,21 @@ class _SplachViewBodyState extends State<SplachViewBody>
         SlidingAnimation(slidingAnimation: slidingAnimation),
       ],
     );
+  }
+
+  void initSlidingAnimation() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+  }
+
+  void goToHome() {
+    Future.delayed(const Duration(seconds: 3), () {
+      Get.to(() => const HomeVeiw(),
+          transition: Transition.fadeIn, duration: kDuration);
+    });
   }
 }
