15dcb73aeabc9984f3250f1325ebeecac4cc830c
ayhamkefo <ayhamkefo048@gmail.com>
2024-09-03 19:22:11 +0300

    added animation to splach page

--- lib/features/splach/presentation/views/widgets/splach_view_body.dart
+++ lib/features/splach/presentation/views/widgets/splach_view_body.dart
@@ -2,9 +2,35 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
 import '../../../../../core/utils/assets_data.dart';
+import 'sliding_animation.dart';
 
-class SplachViewBody extends StatelessWidget {
+class SplachViewBody extends StatefulWidget {
   const SplachViewBody({super.key});
+
+  @override
+  State<SplachViewBody> createState() => _SplachViewBodyState();
+}
+
+class _SplachViewBodyState extends State<SplachViewBody>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+  late Animation<Offset> slidingAnimation;
+  @override
+  void initState() {
+    animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 2));
+    slidingAnimation =
+        Tween<Offset>(begin: const Offset(0, 5), end: Offset.zero)
+            .animate(animationController);
+    animationController.forward();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    animationController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -19,11 +45,7 @@ class SplachViewBody extends StatelessWidget {
         const SizedBox(
           height: 10,
         ),
-        const Text(
-          "Read Free Book",
-          textAlign: TextAlign.center,
-          style: TextStyle(fontSize: 23),
-        ),
+        SlidingAnimation(slidingAnimation: slidingAnimation),
       ],
     );
   }
