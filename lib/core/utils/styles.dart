a543f857692f95dd59638a692dbb495bc35778c6
ayhamkefo <ayhamkefo048@gmail.com>
2024-09-06 14:10:42 +0300

    added the title and subtitle for the books in the books details page

--- lib/core/utils/styles.dart
+++ lib/core/utils/styles.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 abstract class Styles {
+  static const textStyle30 =
+      TextStyle(fontSize: 30, fontWeight: FontWeight.normal);
   static const textStyle20 =
       TextStyle(fontSize: 20, fontWeight: FontWeight.normal);
   static const textStyle18 =
--- lib/features/home/presentation/views/widgets/books_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/books_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:bookly/constants.dart';
+import 'package:bookly/core/utils/styles.dart';
 import 'package:bookly/features/home/presentation/views/widgets/custom_books_limage.dart';
 import 'package:flutter/material.dart';
 
@@ -20,6 +21,15 @@ class BooksDetailsViewBody extends StatelessWidget {
               padding: EdgeInsets.symmetric(
                   horizontal: width * .22, vertical: kPadding),
               child: const CustomBooksImage()),
+          const Text(
+            "The Jungle Book",
+            style: Styles.textStyle30,
+          ),
+          Text(
+            'Rudyard Kipling',
+            style: Styles.textStyle18
+                .copyWith(fontStyle: FontStyle.italic, color: Colors.white54),
+          ),
         ],
       ),
     );
